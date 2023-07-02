0a62aa5b68b8c3dd601d45df11606b509085f4b5
CKDOutcast <morabet.amin@gmail.com>
2023-07-02 17:52:38 +0100

    release markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { marked } from 'marked';
+import './App.scss';
 
 function App() {
+  const [text, setText] = useState(`
+  # This is a Heading 1 Example
+  ## This is a Heading 2 Example
+  [What is renewable energy](https://www.un.org/en/climatechange/what-is-renewable-energy)
+
+  Inline Code example:
+  \`function exampleOfSomething(){
+    return somethingOfValue;
+  }\`
+  \`\`\`
+  Block Code example:
+  {
+    "Movie": "Sisu",
+    "Type": "Action",
+    "Date": 2023
+  }
+  \`\`\`
+  List example:
+  - First Item
+  - Second Item
+  - Third Item
+
+  Blockquote example:
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **This is bold text**
+  `)
+
+  marked.setOptions({breaks: true})
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="h1title">Markdown Previewer</h1>
+        <textarea name="" id="editor" cols="30" rows="10" onChange={(event) => {setText(event.target.value);}}value={text}></textarea>
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
+        
       </header>
     </div>
   );
